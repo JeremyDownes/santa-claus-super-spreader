fbc780fd4bee3071d9c0bd1d4a1808adaa29e81f
Jeremy Downes <jeremydownes@hotmail.com>
2021-09-25 11:24:42 -0400

    significantly improves driving

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -19,37 +19,45 @@ useEffect(()=>{
 
 const keyDown = (e) => {
     dispatchesCopy=dispatches.slice()
-    if(e.keyCode===37){ 
-        dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==rotRight()})
+    if(e.keyCode===37){  
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotRight'})
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotLeft'})
         dispatchesCopy.push(rotLeft())
     }
     if(e.keyCode===39){
-        dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==rotRight()})
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotLeft'})
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotRight'})
         dispatchesCopy.push(rotRight())
     }
     //if(e.keyCode===13){ console.log(e.keyCode) }
     if(e.keyCode===32){ 
-        dispatchesCopy.push(shoot()) 
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/shoot'})
+        dispatchesCopy.push(shoot())
     }
     //if(e.keyCode===37&&e.ctrlKey){ props.registerDispatch(left()); return }
     //if(e.keyCode===39&&e.ctrlKey){ props.registerDispatch(right()); return }  
     if(e.keyCode===38){
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/up'})
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/down'})
         dispatchesCopy.push(up()) 
     } //up
     if(e.keyCode===40){ 
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/up'})
+        dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/down'})
         dispatchesCopy.push(down()) 
     }  //down
+    console.log(dispatchesCopy)
     setDispatches(dispatchesCopy);
   }
 
 const keyUp = (e) => {
     dispatchesCopy=dispatches.slice()
     if(e.keyCode===13){ }
-    if(e.keyCode===32){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==shoot()}) }
-    if(e.keyCode===39) { dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==rotLeft()}) }
-    if(e.keyCode===37) { dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==rotRight()}) }
-    if(e.keyCode===38){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==up()}) } //up
-    if(e.keyCode===40){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c!==down()}) }  //down
+    if(e.keyCode===32){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/shoot'}) }
+    if(e.keyCode===39) { dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotRight'}) }
+    if(e.keyCode===37) { dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/rotLeft'}) }
+    if(e.keyCode===38){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/up'}) } //up
+    if(e.keyCode===40){ dispatchesCopy = dispatchesCopy.filter((c)=>{return c.type!=='input/down'}) }  //down
     setDispatches(dispatchesCopy);        
   }  
 
