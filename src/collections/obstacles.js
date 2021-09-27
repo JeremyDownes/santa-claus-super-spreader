61e32d61bd08570a7442bd9bfb23f659aa6c15a4
Jeremy Downes <jeremydownes@hotmail.com>
2021-09-27 16:23:22 -0400

    adds some objects, refactors object styling, adds some walls

--- src/collections/obstacles.js
+++ src/collections/obstacles.js
@@ -1,3 +1,10 @@
 export const obstacles= [
-	{type: 'fireplace',width:16,height:10,location:[43,0]}
+	{type: 'fireplace',width:13,height:10,location:[45,0]},
+	{type: 'gray-couch-right',width:6,height:10,location:[0,10]},
+	{type: 'blue-chair-back-angle',width:5,height:7,location:[40,12]},
+	{type: 'blue-chair-back-angle reverse',width:5,height:7,location:[60,12]},
+	{type: 'small-table-front',width:4,height:4,location:[1,18]},
+	{type: 'desk-back',width:10,height:8,location:[15,32]},
+	// {type: 'fireplace',width:16,height:10,location:[43,0]},
+
 ]
