1de76b87b14b55ec21120c7e177ed49f9314f0a6
Jeremy Downes <jeremydownes@hotmail.com>
2021-09-27 11:45:39 -0400

    adds and corrects player animations, switches switching to class based and bg to css

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import './Player.css';
-import forward from '../../resources/images/santaforward.gif';
-import forwardRight from '../../resources/images/santaforwardright.gif';
-import forwardLeft from '../../resources/images/santaforwardleft.gif';
-import back from '../../resources/images/santaback.gif';
-import backRight from '../../resources/images/santabackright.gif';
-import backLeft from '../../resources/images/santabackleft.gif';
 
 function Player(props) {
 
 const x = useSelector((state) => state.app.x)
 const y = useSelector((state) => state.app.y)
 const rotation = useSelector((state) => state.app.rotation)
-let image = back
+let bgclass
 
-  if (rotation>338||rotation<=22) {image=back}
-  if (rotation>22&&rotation<=67) {image=backRight}
-  if (rotation>112&&rotation<=158) {image=forwardLeft}    
-  if (rotation>158&&rotation<=202) {image=forward}    
-  if (rotation>202&&rotation<=248) {image=forwardRight}    
-  if (rotation>292&&rotation<=338) {image=backLeft}   
+  if (rotation>338||rotation<=22) {bgclass='back'}
+  if (rotation>22&&rotation<=67) {bgclass='backRight'}
+  if (rotation>67&&rotation<=112) {bgclass='left'}
+  if (rotation>112&&rotation<=158) {bgclass='forwardLeft'}    
+  if (rotation>158&&rotation<=202) {bgclass='forward'}    
+  if (rotation>202&&rotation<=248) {bgclass='forwardRight'}    
+  if (rotation>248&&rotation<=292) {bgclass='right'}    
+  if (rotation>292&&rotation<=338) {bgclass='backLeft'}   
 
 let xstring = x+'vh'
 let ystring = y+'vh'
 
   return (
     <div id="player"  >
-        <div id='piece' style={{top: ystring, left: xstring, zIndex: Math.floor(y), backgroundImage: 'url('+image+')' }}></div>
+        <div id='piece' className={bgclass} style={{top: ystring, left: xstring, zIndex: Math.floor(y)}}></div>
     </div>
         
   );
