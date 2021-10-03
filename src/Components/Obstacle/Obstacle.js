c8a8afd083700edb4264af016a057e7ee474ceb9
Jeremy Downes <jeremydownes@hotmail.com>
2021-10-03 12:02:25 -0400

    adds a bunch more furniture

--- src/Components/Obstacle/Obstacle.js
+++ src/Components/Obstacle/Obstacle.js
@@ -7,10 +7,10 @@ let location = props.location
 
 let x= location[0]+'vh'
 let y= location[1]+'vh'
-let z= location[1]
+let z= Math.round(location[1]+props.height)
 
   return (
-    <div className={"obstacle "+props.type} style={{width: props.width+'vh', height: props.height+'vh',  top: y, left: x, zIndex: location[1]+props.height}} >
+    <div className={"obstacle "+props.type} style={{width: props.width+'vh', height: props.height+'vh',  top: y, left: x, zIndex: z}} >
     </div>
   );
 }
--- src/collections/obstacles.js
+++ src/collections/obstacles.js
@@ -4,16 +4,30 @@ export const obstacles= [
 	{type: 'blue-chair-back-angle',width:5,height:7,location:[40,12]},
 	{type: 'blue-chair-back-angle reverse',width:5,height:7,location:[60,12]},
 	{type: 'small-table-front',width:4,height:4,location:[2,18]},
+
 	{type: 'desk-back',width:10,height:8,location:[15,32]},
 	{type: 'bed-side reverse',width:16,height:7,location:[1,50]},
+
 	{type: 'bed-2-side',width:16,height:7,location:[83,50]},
-	{type: 'cabinet',width:4,height:10,location:[65,70]},
-	{type: 'cabinet',width:4,height:10,location:[68.5,70]},
-	{type: 'stove',width:6,height:6,location:[72,74]},
-	{type: 'cabinet',width:4,height:10,location:[78,70]},
-	{type: 'cabinet',width:4,height:10,location:[81.5,70]},
-	{type: 'fridge',width:6,height:9,location:[85.5,71]},
+
+	{type: 'cabinet',width:4,height:10,location:[62,70]},
+	{type: 'cabinet',width:4,height:10,location:[65.5,70]},
+	{type: 'stove',width:6,height:6,location:[69,74]},
+	{type: 'cabinet',width:4,height:10,location:[75,70]},
+	{type: 'cabinet',width:4,height:10,location:[78.5,70]},
+	{type: 'fridge',width:6,height:9,location:[82.5,71]},
+	{type: 'cabinet',width:4,height:10,location:[89,70]},
+	{type: 'corner-cabinet',width:7,height:12,location:[92,70]},
+	{type: 'sink-cabinet-side reverse',width:5,height:13,location:[94.5,70]},
+	{type: 'cabinet-side reverse',width:5.5,height:14,location:[94.5,70.8]},
+	{type: 'wooden-chair',width:3,height:6,location:[70,83]},
+	{type: 'wooden-chair',width:3,height:6,location:[75,83]},
+	{type: 'wooden-chair-back',width:3,height:6,location:[69,86]},
+	{type: 'wooden-chair-back',width:3,height:6,location:[76,86]},
 	{type: 'table',width:12,height:6,location:[68,85]},
-	{type: 'toilet-side',width:5,height:5,location:[1,87]},
+
+	{type: 'tub',width:8,height:18,location:[1,76]},
+	{type: 'toilet',width:4,height:6,location:[15,78]},
+	{type: 'sink',width:6,height:6,location:[25,78]},
 
 ]
